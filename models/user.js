955e2de30def571d11f5ced08390446f4fbcf42f
23carnies <46233990+23carnies@users.noreply.github.com>
2020-08-28 11:20:43 -0400

    Update userSchema, add goalSchema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
+const goalSchema = new Schema ({
+  title: String,
+  content: String,
+  goalDate: Date
+})
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   name: String,
   email: String,
   avatar: String,
   googleId: String,
   bio: String,
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User'}],
+  goalSchema: [goalSchema]
 }, {
   timestamps: true
 });
